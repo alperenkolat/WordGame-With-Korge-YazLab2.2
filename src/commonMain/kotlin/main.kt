5fa62d0e2c72f173a05e68434ded26b17afe127b
Alperen Kolat <73352908+alperenkolat@users.noreply.github.com>
2023-04-13 23:44:15 +0300

    feat: button tasarımları yapıldı

--- src/commonMain/kotlin/main.kt
+++ src/commonMain/kotlin/main.kt
@@ -1,3 +1,4 @@
+import com.soywiz.kds.*
 import com.soywiz.klock.*
 import com.soywiz.korge.*
 import com.soywiz.korge.scene.*
@@ -9,8 +10,9 @@ import com.soywiz.korio.file.std.*
 import com.soywiz.korma.geom.*
 import com.soywiz.korma.interpolation.*
 import com.soywiz.korge.Korge
-import com.soywiz.korge.box2d.registerBodyWithFixture
+import com.soywiz.korge.box2d.*
 import com.soywiz.korge.input.onClick
+import com.soywiz.korge.ui.*
 import com.soywiz.korge.view.position
 import com.soywiz.korge.view.rotation
 import com.soywiz.korge.view.solidRect
@@ -18,11 +20,13 @@ import com.soywiz.korgw.GameWindow
 import com.soywiz.korim.color.Colors
 import com.soywiz.korma.geom.Angle
 import com.soywiz.korma.geom.degrees
+import com.soywiz.korma.random.*
 import org.jbox2d.dynamics.BodyType
+import org.jbox2d.dynamics.joints.*
 import kotlin.random.Random
 
 
-suspend fun main() = Korge(width = 512, height = 1280, bgcolor = Colors["#2b2b2b"]) {
+suspend fun main() = Korge(width = 512, height =1280, bgcolor = Colors["#2b2b2b"]) {
     val sceneContainer = sceneContainer()
 
     sceneContainer.changeTo({ MyScene() })
@@ -30,38 +34,64 @@ suspend fun main() = Korge(width = 512, height = 1280, bgcolor = Colors["#2b2b2b
 
 class MyScene : Scene() {
     override suspend fun SContainer.sceneMain() {
+        val random1 = Random(0L)
 
 
-        // Kırmızı bir dikdörtgen oluşturulur, boyutu 50x50 piksel, konumu (400, 50), dönüş açısı 30 derece.
-        // Kutu, dinamik bir cisim olarak kaydedilir, yoğunluğu 2, sürtünmesi 0.01 olarak ayarlanır.
-        solidRect(50, 50, Colors.RED).position(400, 50).rotation(30.degrees)
-            .registerBodyWithFixture(type = BodyType.DYNAMIC, density = 2, friction = 0.01)
 
-        // Kırmızı bir dikdörtgen oluşturulur, boyutu 50x50 piksel, konumu (300, 100).
-        // Kutu, dinamik bir cisim olarak kaydedilir.
-        solidRect(50, 50, Colors.RED).position(300, 100).registerBodyWithFixture(type = BodyType.DYNAMIC)
-
-        // Kırmızı bir dikdörtgen oluşturulur, boyutu 50x50 piksel, konumu (450, 100), dönüş açısı 15 derece.
-        // Kutu, dinamik bir cisim olarak kaydedilir.
-        solidRect(50, 50, Colors.RED).position(450, 100).rotation(15.degrees)
-            .registerBodyWithFixture(type = BodyType.DYNAMIC)
-
-        // Beyaz bir dikdörtgen oluşturulur, boyutu 600x100 piksel, konumu (100, 600).
-        // Kutu, statik bir cisim olarak kaydedilir, sürtünmesi 0.2 olarak ayarlanır.
-        solidRect(600, 100, Colors.WHITE).position(100, 600).registerBodyWithFixture(
+        solidRect(512, 50, Colors.WHITE).position(0, 1050).registerBodyWithFixture(
             type = BodyType.STATIC,
-            friction = 0.2
+            friction = 0.99
+        )
+        for (j in 1..3)
+            for (i in 1..8){
+
+                uiButton(width=50.0, 50.0,text = "B") {
+                    position(500-i*50, 450).rotation(0.degrees)
+                    onPress { println("TAPPED ON 1") }
+                }.also {  it.colorMul =   random1 [random1[Colors.CYAN, Colors.WHITE],random1[Colors.YELLOW, Colors.CYAN]]}
+                    .registerBodyWithFixture(type = BodyType.DYNAMIC, density = 10.0, friction = 0.99)
+                    .also {  it.textColor =  Colors.AZURE}
+            }
+
+        //silme butonu
+        uiButton(width=100.0, 60.0,text = "X", ) {
+            position(0, 1050)
+            onPress { println("TAPPED ON 1") }
+        }.also {  it.colorMul =  Colors["#ff1552"]}
+            .also {  it.textColor =  Colors.WHITE}
+
+
+        //onaylama butonu
+        uiButton(width=100.0, 60.0,text = "OK") {
+            position(410, 1050)
+            onPress { println("TAPPED ON 1") }
+        }.also {  it.colorMul =  Colors["#4effeb"]}
+            .also {  it.textColor =  Colors.WHITE}
+        //direk
+        solidRect(3, 450, Colors.BLACK).position(93, 650).registerBodyWithFixture(
+            type = BodyType.STATIC,
+            friction = 0.99
+        ) //direk
+        solidRect(3, 450, Colors.BLACK).position(503, 650).registerBodyWithFixture(
+            type = BodyType.STATIC,
+            friction = 0.99
         )
         onClick {
-            val pos = it.currentPosLocal // Tıklama olayından gelen pozisyonu alır
-            solidRect(50, 50, Colors.RED).position(pos.x, pos.y)
-                .rotation(randomAngle()) // 50x50 boyutunda, kırmızı bir dikdörtgen oluşturur ve tıklama pozisyonuna yerleştirir. Ayrıca rastgele bir açı ile döndürür.
-                .registerBodyWithFixture(type = BodyType.DYNAMIC) // Oluşturulan kutunun Box2D dünyasına dinamik bir cisim olarak kaydedilmesi sağlanır.
+            for (i in 1..8){
+
+                uiButton(width=50.0, 50.0,text = "A") {
+                    position(500-i*50, 450).rotation(0.degrees)
+                    onPress { println("TAPPED ON 1") }
+                    buttonBackColor=Colors.WHITE
+                }.also {  it.colorMul =   random1 [random1[Colors.RED, Colors.AQUA],random1[Colors.YELLOW, Colors.BLUE]]}
+                    .registerBodyWithFixture(type = BodyType.DYNAMIC, density = 10.0, friction = 0.99)
+
+            }
+
         }
 
-// Tıklama olayı eklenir. Tıklanan yere, boyutu 50x50 piksel olan kırmızı bir dikdörtgen yerleştirilir.
-// Kutu, dinamik bir cisim olarak kaydedilir, dönüş
     }
 
+
 }
-fun randomAngle(): Angle = Random.nextInt(0, 90).degrees // Rastgele bir açı değeri döndüren fonksiyon. Açı derecesi, 0 ila 90 arasında rastgele seçilir.
+fun randomAngle(): Angle = Random.nextInt(0, 90).degrees // Rastgele bir açı değeri döndüren fonksiyon. Açı derecesi, 0 ila 90 arasında rastgele seçilir.
