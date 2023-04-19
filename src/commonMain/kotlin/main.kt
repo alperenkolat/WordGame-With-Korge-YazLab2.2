414abff9d7e5b68e5872ef6a991cc6d74d1b49de
StartaBafras <beyhankaradag00@gmail.com>
2023-04-19 14:49:17 +0300

    Kelime kontrolü eklendi

--- src/commonMain/kotlin/main.kt
+++ src/commonMain/kotlin/main.kt
@@ -1,97 +1,307 @@
-import com.soywiz.kds.*
-import com.soywiz.klock.*
-import com.soywiz.korge.*
 import com.soywiz.korge.scene.*
-import com.soywiz.korge.tween.*
 import com.soywiz.korge.view.*
-import com.soywiz.korim.color.*
-import com.soywiz.korim.format.*
-import com.soywiz.korio.file.std.*
-import com.soywiz.korma.geom.*
-import com.soywiz.korma.interpolation.*
 import com.soywiz.korge.Korge
 import com.soywiz.korge.box2d.*
+import com.soywiz.korge.box2d.body
+import com.soywiz.korge.debug.*
 import com.soywiz.korge.input.onClick
 import com.soywiz.korge.ui.*
 import com.soywiz.korge.view.position
 import com.soywiz.korge.view.rotation
+import com.soywiz.korge.view.roundRect
 import com.soywiz.korge.view.solidRect
-import com.soywiz.korgw.GameWindow
 import com.soywiz.korim.color.Colors
-import com.soywiz.korma.geom.Angle
+import com.soywiz.korim.font.*
+import com.soywiz.korim.paint.*
+import com.soywiz.korio.dynamic.*
+import com.soywiz.korio.file.std.*
 import com.soywiz.korma.geom.degrees
+import com.soywiz.korma.geom.vector.*
 import com.soywiz.korma.random.*
-import org.jbox2d.dynamics.BodyType
-import org.jbox2d.dynamics.joints.*
+import kotlinx.coroutines.*
+import org.jbox2d.dynamics.*
+import com.soywiz.korge.resources.*
+
+import kotlin.concurrent.*
 import kotlin.random.Random
+
+import java.io.*
 
 
 suspend fun main() = Korge(width = 512, height =1280, bgcolor = Colors["#2b2b2b"]) {
     val sceneContainer = sceneContainer()
-
     sceneContainer.changeTo({ MyScene() })
 }
-
 class MyScene : Scene() {
-    override suspend fun SContainer.sceneMain() {
-        val random1 = Random(0L)
-
-
-
-        solidRect(512, 50, Colors.WHITE).position(0, 1050).registerBodyWithFixture(
-            type = BodyType.STATIC,
-            friction = 0.99
-        )
-        for (j in 1..3)
-            for (i in 1..8){
-
-                uiButton(width=50.0, 50.0,text = "B") {
-                    position(500-i*50, 450).rotation(0.degrees)
-                    onPress { println("TAPPED ON 1") }
-                }.also {  it.colorMul =   random1 [random1[Colors.CYAN, Colors.WHITE],random1[Colors.YELLOW, Colors.CYAN]]}
-                    .registerBodyWithFixture(type = BodyType.DYNAMIC, density = 10.0, friction = 0.99)
-                    .also {  it.textColor =  Colors.AZURE}
+    val destiny=10.0
+    val boxSize=49.0
+    lateinit var scoreText: Text // skor kutusunu tutacak değişken
+    lateinit var InputText:Text
+    var contText="beyhan ;)"
+    var score1 = 0 // skoru tutacak değişken
+    val contentInput=ArrayList<String>()
+    var delayTime=5000L
+    override suspend fun SContainer.sceneInit() {
+        val font = resourcesVfs["BebasNeue-Regular.ttf"].readTtfFont()
+        graphics {
+            it.position(0.0,350.0)
+            fill(Colors["#474748"]) {
+                roundRect(0.0, 0.0, 512.0, 200.0, 5.0)
             }
-
-        //silme butonu
-        uiButton(width=100.0, 60.0,text = "X", ) {
-            position(0, 1050)
-            onPress { println("TAPPED ON 1") }
-        }.also {  it.colorMul =  Colors["#ff1552"]}
-            .also {  it.textColor =  Colors.WHITE}
-
-
-        //onaylama butonu
-        uiButton(width=100.0, 60.0,text = "OK") {
-            position(410, 1050)
-            onPress { println("TAPPED ON 1") }
-        }.also {  it.colorMul =  Colors["#4effeb"]}
-            .also {  it.textColor =  Colors.WHITE}
-        //direk
-        solidRect(3, 450, Colors.BLACK).position(93, 650).registerBodyWithFixture(
-            type = BodyType.STATIC,
-            friction = 0.99
-        ) //direk
-        solidRect(3, 450, Colors.BLACK).position(503, 650).registerBodyWithFixture(
-            type = BodyType.STATIC,
-            friction = 0.99
-        )
-        onClick {
-            for (i in 1..8){
-
-                uiButton(width=50.0, 50.0,text = "A") {
-                    position(500-i*50, 450).rotation(0.degrees)
-                    onPress { println("TAPPED ON 1") }
-                    buttonBackColor=Colors.WHITE
-                }.also {  it.colorMul =   random1 [random1[Colors.RED, Colors.AQUA],random1[Colors.YELLOW, Colors.BLUE]]}
-                    .registerBodyWithFixture(type = BodyType.DYNAMIC, density = 10.0, friction = 0.99)
-
-            }
-
         }
+        val bgScore= circle(80.0,  Colors["#75888c"]) {
+            position(180.0, 370.0)
+        }
+        scoreText = text("$score1", textSize = 32.0,  ).centerOn(bgScore)
+
+         val bgInput=solidRect(412, 50, Colors.DARKGOLDENROD).position(0, 1050)
+             .registerBodyWithFixture(
+             type = BodyType.STATIC,
+             friction = 0.99
+         )
+        InputText =text("$contText", textSize = 30.0,Colors.WHITE,font)
+            .centerOn(bgInput)
+
 
     }
 
+    override suspend fun SContainer.sceneMain() {
+        val file = resourcesVfs["word_list.txt"]
+        val lines = file.readLines()
+
+
+        val random1 = Random(0L)
+        val font = resourcesVfs["BebasNeue-Regular.ttf"].readTtfFont()
+        val cellSize = views.virtualWidth / 10.0
+        val fieldSize = 50 + 8 * cellSize
+        val leftIndent = (views.virtualWidth - fieldSize) / 2
+        val topIndent = 150.0
+        val charr = "ABCDEFGHIJKLMNOPRSTUVYZÇĞİÖŞÜ"
+        val charrList = charr.split("").filter { it.isNotEmpty() }.toList() as ArrayList<String>
+        var falseCount= 0
+
+        val buttonList = arrayListOf<UIButton>()
+        charrList.shuffle()
+        solidRect(512, 50, Colors.DARKGOLDENROD).position(0, 1050).registerBodyWithFixture(
+            type = BodyType.STATIC,
+            friction = 0.99
+        ).visible=false
+
+        graphics {
+            it.position(0.0,550.0)
+            fill(Colors["#cec0b2"]) {
+                roundRect(0.0, 0.0, 512.0, 500.0, 5.0)
+            }
+        }
+
+//      val bgScore= circle(80.0,  Colors["#75888c"]) {
+//            position(180.0, 370.0)
+//        }
+//        text("", cellSize * 0.5, Colors.WHITE, ).centerOn(bgScore)
+
+        //süs
+      solidRect(240.0,7.0,  Colors.GOLD) {
+            position(140.0, 550.0)
+        }
+
+        //direk
+        for (i in 0..8)
+            solidRect(3, 470, Colors.BLACK).position(2+i*63, 600).registerBodyWithFixture(
+                type = BodyType.STATIC,
+                friction = 0.99
+            ).visible=false
+        //silme butonu
+        val  removeBttn=  uiButton(width = 105.0, 65.0, text = "") {
+            position(-1, 1045)
+            onPress {
+                if (buttonList.isNotEmpty()) {
+                    // Listenin son elemanını sil
+                    for (k in 0..buttonList.size-1){
+                    val btn = buttonList.removeAt(buttonList.size - 1)
+                    btn.colorMul = random1[random1[Colors.CYAN, Colors.WHITE], random1[Colors.YELLOW, Colors.CYAN]]
+                }}
+                contentInput.clear()
+                UpdateContent("")
+                println(contentInput.joinToString(separator = ""))  }
+        }.also { it.colorMul = Colors["#ff1552"] }
+            .also { it.textColor = Colors.WHITE }
+            .also { it.textSize =cellSize/1.5 }
+        text("X", cellSize * 0.5, Colors.WHITE, font).centerOn(removeBttn)
+
+
+        //onaylama butonu
+        val onay=uiButton(width = 105.0, 65.0, text = "OK") {
+            position(410, 1045)
+            onPress {
+
+                for (line in lines) {
+                    if (line.equals(contentInput.joinToString(separator = "")))
+                    {
+                        calculateWordPoints(contentInput.joinToString(separator = ""))
+                    }
+                }
+
+
+            if(contentInput.joinToString(separator = "")=="DD"){//??
+
+                falseCount+=1
+                println(falseCount)
+                if(falseCount==3) {
+                    falseCount = 0
+                    for (i in 0..7) {
+                    val charRand = Random.nextInt(0, charrList.size)
+                    val newb = uiButton(width = boxSize, boxSize, text = charrList[charRand]) {
+//                        position(-397 + i * 63, -600).rotation(0.degrees)
+                         position(13+ i * 63,450 ).rotation(0.degrees)
+                        onPress {
+                            println(charrList[charRand])
+                            UpdateContent(charrList[charRand])
+                            buttonList.add(this)
+                            this.colorMul=Colors.WHITE
+                            for (line in lines) {
+                                if (line.equals(contentInput.joinToString(separator = "")))
+                                {
+                                    calculateWordPoints(contentInput.joinToString(separator = ""))
+                                }
+                            }
+
+
+
+                        }
+                    }.also {it.colorMul = random1[random1[Colors.CYAN, Colors.WHITE], random1[Colors.YELLOW, Colors.CYAN]] }
+                        .also { it.textColor = Colors.AZURE }
+                        .also { it.textSize = cellSize / 2 }
+                        .also { it.textFont = font }
+                    val a=newb.parent?.parent
+                     a?.addChild(newb)
+                    newb.registerBodyWithFixture(type = BodyType.DYNAMIC,density = destiny,friction = 100.0,
+                        angularDamping = 50.0,
+                        gravityScale = 2.0,
+                    )
+
+                   }}
+               }else{
+                if (buttonList.isNotEmpty()) {
+                    // Listenin son elemanını sil
+                    for (k in 0..buttonList.size-1){
+                        val btn = buttonList.removeAt(buttonList.size - 1)
+                        btn.position(440.0,4004.0)
+                        contentInput.clear()
+                        UpdateContent("")
+                    }}
+               }
+            }
+        }.also { it.colorMul = Colors["#4effeb"] }
+            .also { it.textColor = Colors.WHITE }
+            .also { it.textSize =cellSize/1.5 }
+
+
+   for (j in 0..2){
+            for (i in 0..7) {
+                val  charRand = Random.nextInt(0,charrList.size)
+              val alp=  uiButton(width = boxSize, boxSize, text = charrList[charRand]) {
+                    position(13+ i * 63, 450).rotation(0.degrees)
+
+                    onClick {
+
+                        println(charrList[charRand])
+                        UpdateContent(charrList[charRand])
+                        buttonList.add(this)
+                        this.colorMul=Colors.WHITE
+                    }
+                }.also {it.colorMul = random1[random1[Colors.CYAN, Colors.WHITE], random1[Colors.YELLOW, Colors.CYAN]]
+                }.registerBodyWithFixture(type = BodyType.DYNAMIC, density = destiny, friction = 100.0,angularDamping = 50.0, gravityScale = 2.0)
+                    .also { it.textColor = Colors.AZURE }
+                    .also { it.textSize =cellSize/2 }
+                  .also { it.textFont =font }
+
+            }}
+
+            //süreye göre harf ekleme
+            GlobalScope.launch {
+            while(true) {
+
+                delay(delayTime)
+
+                val  charRand = Random.nextInt(0,charrList.size)
+                val i = Random.nextInt(0, 8)
+                val alp=  uiButton(width = boxSize, boxSize, text = charrList[charRand]) {
+                    position(13+ i * 63, 450).rotation(0.degrees)
+
+                    onPress {
+                        println(charrList[charRand])
+                        UpdateContent(charrList[charRand])
+                        buttonList.add(this)
+                        this.colorMul=Colors.WHITE
+                    }
+                }.also {it.colorMul = random1[random1[Colors.CYAN, Colors.WHITE], random1[Colors.YELLOW, Colors.CYAN]]
+                }.registerBodyWithFixture(type = BodyType.DYNAMIC, density = destiny, friction = 100.0,angularDamping = 50.0, gravityScale = 2.0)
+                    .also { it.textColor = Colors.AZURE }
+                    .also { it.textSize =cellSize/2 }
+                    .also { it.textFont =font }
+            }
+        }
+    }
+
+
+    fun UpdateContent(newChar: String) {
+        contentInput.add(newChar)
+        InputText.text = "${contentInput.joinToString(separator = "")}" // skor kutusunu güncelle
+    }
+    fun updateScore(newScore: Int) {
+        score1 += newScore // skoru güncelle
+        //süre azaltma
+             if(score1>=400)
+                delayTime=1000L
+             else if(score1>=300)
+                delayTime=2000L
+             else if(score1>=300)
+                delayTime=3000L
+             else if(score1>=200)
+                delayTime=3000L
+             else  if(score1>=100)
+                delayTime=4000L
+
+        scoreText.text = "$score1" // skor kutusunu güncelle
+   }
+    fun calculateWordPoints(word: String) {
+        var points = 0
+        for (char in word) {
+            points += when (char.toUpperCase()) {
+                'A' -> 1
+                'B' -> 3
+                'C' -> 4
+                'Ç' -> 4
+                'D' -> 3
+                'E' -> 1
+                'F' -> 7
+                'G' -> 5
+                'Ğ' -> 8
+                'H' -> 5
+                'I' -> 2
+                'İ' -> 1
+                'J' -> 10
+                'K' -> 1
+                'L' -> 1
+                'M' -> 2
+                'N' -> 1
+                'O' -> 2
+                'Ö' -> 7
+                'P' -> 5
+                'R' -> 1
+                'S' -> 2
+                'Ş' -> 4
+                'T' -> 1
+                'U' -> 2
+                'Ü' -> 3
+                'V' -> 7
+                'Y' -> 3
+                'Z' -> 4
+                else -> 0
+            }
+        }
+        updateScore(points)
+    }
 
 }
-fun randomAngle(): Angle = Random.nextInt(0, 90).degrees // Rastgele bir açı değeri döndüren fonksiyon. Açı derecesi, 0 ila 90 arasında rastgele seçilir.
+
